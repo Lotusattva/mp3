88e51b36b4ddc5ea5a8e70bed97734aa10473f25
Jingwei Chen <cjw990802@gmail.com>
2024-11-20 13:27:31 -0800

    merge

--- src/main/java/fsft/wikipedia/WikiMediator.java
+++ src/main/java/fsft/wikipedia/WikiMediator.java
@@ -1,25 +1,5 @@
 package fsft.wikipedia;
 
-<<<<<<< HEAD
-public class WikiMediator {
-
-    /* TODO: Implement this datatype
-
-        You must implement the methods with the exact signatures
-        as provided in the statement for this mini-project.
-
-        You must add method signatures even for the methods that you
-        do not plan to implement. You should provide skeleton implementation
-        for those methods, and the skeleton implementation could return
-        values like null.
-
-        You can add constructors that could help with your implementation of
-        WikiMediatorServer but there should be a default constructor that takes
-        no arguments for Task 3.
-
-     */
-
-=======
 import java.util.List;
 import java.time.Duration;
 import java.util.Map;
@@ -123,5 +103,4 @@ public class WikiMediator {
     public int peakLoad(Duration duration) {
         return (int) searchRecord.keySet().stream().filter(i -> i.isAfter(Instant.now().minus(duration))).count();
     }
->>>>>>> 06307f0 (update)
 }
